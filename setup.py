c8c1817a431624ca0de63b4206b73294c4da645c
Jonas Hagstedt <hagstedt@gmail.com>
2013-12-13 02:06:24 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="simple blog",
+    version="0.1.0",
+    author="Jonas Hagstedt",
+    author_email="hagstedt@gmail.com",
+    description=("A VERY basic blog"),
+    license="BSD",
+    keywords="django blog",
+    url = "https://github.com/jonashagstedt/super-simple-django-blog",
+    packages=['simple_blog', ],
+    long_description=read('README.md'),
+    install_requires=[
+        "Django >= 1.4",
+        "django-taggit"
+    ],
+    classifiers=[
+        "Development Status :: Beta",
+        "Topic :: Blog",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
